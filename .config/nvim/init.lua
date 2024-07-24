0cb5f684c608da93e551caf5d6a4d89e629a6738
insidesources <56140662+insidesources@users.noreply.github.com>
2024-07-24 18:25:50 -0400

    Delete .config/nvim directory

--- .config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("config.lazy")
-require("lazy").setup("plugins")
-vim.cmd("colorscheme onedark")
--- bufferline
-vim.opt.termguicolors = true
-require("bufferline").setup{}
--- .config/nvim/lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',      
-dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-	      local builtin = require("telescope.builtin")
-	      vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-      end
-    }
