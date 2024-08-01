8d084861c39f083bb2f4f00524eb8890210e8329
insideosurces <insidesources@pm.me>
2024-08-01 00:53:40 -0400

    deleted line 3 in colors.py

--- .config/qtile/colors.py
+++ .config/qtile/colors.py
@@ -1,6 +1,5 @@
 # If using transparency, make sure you add (background="#00000000") to 'Screen' line(s).
 # Then, you can use RGBA color codes to add transparency to the colors below.
-# For ex: colors = [["#282c34ee", "#282c34dd"], ...
 
 DoomOne = [
     ["#282c34", "#282c34"], # bg
