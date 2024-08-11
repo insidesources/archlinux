647bb5436f7c5bfc5dab4bab4a1f7f2e250df6d9
insidesources <insidesources@pm.me>
2024-08-10 23:02:11 -0400

    changed bufferline.lua config file

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,1 +1,21 @@
-return {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'}
+return {'akinsho/bufferline.nvim',
+	version = "*",
+	dependencies = 'nvim-tree/nvim-web-devicons',
+	config = function()
+		require("bufferline").setup({
+			options = {
+				buffer_close_icon = "⨉",
+				modified_icon = "●",
+				close_icon = "⨉",
+				show_buffer_icons = true,
+				show_buffer_close_icons = true,
+				show_close_icon = true,
+				show_tab_indicators = true,
+				separator_style = "thin",
+				indicator = {
+					style = 'none',
+				},
+			},
+	        })
+            end
+       }
